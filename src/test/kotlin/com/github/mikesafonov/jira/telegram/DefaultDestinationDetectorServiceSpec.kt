c6307d6d2244aa88d28228caa8cfa51620f391c1
msafonov <msafonovmail@gmail.com>
2019-02-24 19:10:57 +0300

    DefaultDestinationDetectorService tests

--- src/test/kotlin/com/github/mikesafonov/jira/telegram/DefaultDestinationDetectorServiceSpec.kt
+++ src/test/kotlin/com/github/mikesafonov/jira/telegram/DefaultDestinationDetectorServiceSpec.kt
@@ -1,10 +1,11 @@
 package com.github.mikesafonov.jira.telegram
 
 import com.github.mikesafonov.jira.telegram.config.ApplicationProperties
-import com.github.mikesafonov.jira.telegram.dto.Event
-import com.github.mikesafonov.jira.telegram.dto.WebHookEvent
+import com.github.mikesafonov.jira.telegram.dto.IssueEventTypeName
+import com.github.mikesafonov.jira.telegram.generators.*
 import com.github.mikesafonov.jira.telegram.service.destination.DefaultDestinationDetectorService
 import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.BehaviorSpec
 import io.mockk.every
 import io.mockk.mockk
@@ -15,24 +16,114 @@ import io.mockk.mockk
 class DefaultDestinationDetectorServiceSpec : BehaviorSpec({
 
     val applicationProperties = mockk<ApplicationProperties>()
-    lateinit var defaultDestinationDetectorService: DefaultDestinationDetectorService
+    val defaultDestinationDetectorService = DefaultDestinationDetectorService(applicationProperties)
 
     Given("Destination service with flag sendToMe = true") {
         every { applicationProperties.notification.sendToMe } returns true
-        defaultDestinationDetectorService = DefaultDestinationDetectorService(applicationProperties)
 
         When("Event without issue field") {
-            val event = Event(WebHookEvent.COMMENT_CREATED, null, 10000L, null, null, null, null)
+            val event = EventGen().generateOne(issue = null)
             Then("Return empty list") {
                 defaultDestinationDetectorService.findDestinations(event) shouldHaveSize 0
+            }
+        }
 
+        When("Event without issueEventTypeName field") {
+            val event = EventGen().generateOne(issueEventTypeName = null)
+            Then("Return empty list") {
+                defaultDestinationDetectorService.findDestinations(event) shouldHaveSize 0
+            }
+        }
+
+        When("Any issue Event") {
+            Then("Return list of creator, reporter and assignee names") {
+                IssueEventTypeName.values().forEach {
+                    val event = EventGen().generateOne(issueEventTypeName = it)
+                    val expectedNames =
+                        listOfNotNull(event.issue?.creatorName, event.issue?.reporterName, event.issue?.assigneeName)
+                    val destinations = defaultDestinationDetectorService.findDestinations(event)
+
+                    destinations shouldBe expectedNames
+                }
             }
         }
     }
 
-//    Given("Destination service with flag sendToMe = false") {
-//        every { applicationProperties.notification.sendToMe } returns false
-//        defaultDestinationDetectorService = DefaultDestinationDetectorService(applicationProperties)
-//    }
+    Given("Destination service with flag sendToMe = false") {
+        every { applicationProperties.notification.sendToMe } returns false
+
+        When("Event without issue field") {
+            val event = EventGen().generateOne(issue = null)
+            Then("Return empty list") {
+                defaultDestinationDetectorService.findDestinations(event) shouldHaveSize 0
+            }
+        }
+
+        When("Event without issueEventTypeName field") {
+            val event = EventGen().generateOne(issueEventTypeName = null)
+            Then("Return empty list") {
+                defaultDestinationDetectorService.findDestinations(event) shouldHaveSize 0
+            }
+        }
+
+        When("ISSUE_COMMENTED issue Event and comment author and issue creator are same") {
+            val authorUser = UserGen.generateDefault()
+            val event = EventGen().generateOne(
+                issueEventTypeName = IssueEventTypeName.ISSUE_COMMENTED,
+                issue = IssueGen().generateOne(issueFields = IssueFieldsGen().generateOne(creator = authorUser)),
+                comment = CommentGen().generateOne(author = authorUser)
+            )
+            Then("Return list of reporter and assignee names") {
+                val expectedNames =
+                    listOfNotNull(event.issue?.reporterName, event.issue?.assigneeName)
+                val destinations = defaultDestinationDetectorService.findDestinations(event)
+
+                destinations shouldBe expectedNames
+            }
+        }
+
+        When("ISSUE_CREATED issue Event and event author and issue creator are same") {
+            val authorUser = UserGen.generateDefault()
+            val event = EventGen().generateOne(
+                issueEventTypeName = IssueEventTypeName.ISSUE_CREATED,
+                user = authorUser,
+                issue = IssueGen().generateOne(issueFields = IssueFieldsGen().generateOne(creator = authorUser))
+            )
+            Then("Return list of reporter and assignee names") {
+                val expectedNames =
+                    listOfNotNull(event.issue?.reporterName, event.issue?.assigneeName)
+                val destinations = defaultDestinationDetectorService.findDestinations(event)
+
+                destinations shouldBe expectedNames
+            }
+        }
+
+        When("ISSUE_UPDATED issue Event and event author and issue creator are same") {
+            val authorUser = UserGen.generateDefault()
+            val event = EventGen().generateOne(
+                issueEventTypeName = IssueEventTypeName.ISSUE_UPDATED,
+                user = authorUser,
+                issue = IssueGen().generateOne(issueFields = IssueFieldsGen().generateOne(creator = authorUser))
+            )
+            Then("Return list of reporter and assignee names") {
+                val expectedNames =
+                    listOfNotNull(event.issue?.reporterName, event.issue?.assigneeName)
+                val destinations = defaultDestinationDetectorService.findDestinations(event)
+
+                destinations shouldBe expectedNames
+            }
+        }
+
+        When("ISSUE_COMMENT_EDITED,ISSUE_COMMENT_DELETED, ISSUE_GENERIC issue Event") {
+            val event = EventGen.generateDefault()
+            Then("Return list of creator, reporter and assignee names") {
+                val expectedNames =
+                    listOfNotNull(event.issue?.creatorName, event.issue?.reporterName, event.issue?.assigneeName)
+                val destinations = defaultDestinationDetectorService.findDestinations(event)
+
+                destinations shouldBe expectedNames
+            }
+        }
+    }
 
 })

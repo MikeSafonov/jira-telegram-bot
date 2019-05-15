8f02401fe5fcd60146cfeefe1c83cdfad7ce311a
msafonov <msafonovmail@gmail.com>
2019-05-15 15:48:27 +0300

    refactor: remove unnecessary CommandHandler

--- src/main/kotlin/com/github/mikesafonov/jira/telegram/service/telegram/TelegramCommandExecutor.kt
+++ src/main/kotlin/com/github/mikesafonov/jira/telegram/service/telegram/TelegramCommandExecutor.kt
@@ -1,9 +1,8 @@
 package com.github.mikesafonov.jira.telegram.service.telegram
 
 import com.github.mikesafonov.jira.telegram.dao.ChatRepository
+import com.github.mikesafonov.jira.telegram.dao.State
 import com.github.mikesafonov.jira.telegram.service.telegram.handlers.TelegramCommandHandler
-import com.github.mikesafonov.jira.telegram.service.telegram.handlers.UnknownChatTelegramCommandHandler
-import com.github.mikesafonov.jira.telegram.service.telegram.handlers.UnknownCommandTelegramCommandHandler
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 import org.telegram.telegrambots.meta.api.objects.Message
@@ -20,22 +19,50 @@ class TelegramCommandExecutor(
 
     fun execute(command: TelegramCommand, body: (BotApiMethod<Message>) -> Unit) {
         val commandResponse = if (command.chat == null) {
-            UnknownChatTelegramCommandHandler(telegramMessageBuilder).handle(command)
+            unknownChatResponse(command)
         } else {
-            val handler = handlers.find { it.isHandle(command) }
-            if (handler == null) {
-                UnknownCommandTelegramCommandHandler(telegramMessageBuilder).handle(command)
-            } else {
-                handler.handle(command)
-            }
+            executeByHandler(command)
         }
 
-        if(command.chat != null && command.chat.state != commandResponse.nextState){
-            command.chat.state = commandResponse.nextState
-            chatRepository.save(command.chat)
-        }
-
+        updateChatState(command, commandResponse)
 
         body(commandResponse.method)
     }
+
+    /**
+     * Find first handler for [command] and handle it. If no handlers return unknown command response
+     */
+    private fun executeByHandler(command: TelegramCommand): TelegramCommandResponse {
+        val handler = handlers.find { it.isHandle(command) }
+        return handler?.handle(command) ?: unknownCommandResponse(command)
+    }
+
+    /**
+     * If [commandResponse]`s nextState is different to [command]`s chat state then changes chat state in database
+     */
+    private fun updateChatState(command: TelegramCommand, commandResponse: TelegramCommandResponse) {
+        if (command.chat != null && command.chat.state != commandResponse.nextState) {
+            command.chat.state = commandResponse.nextState
+            chatRepository.save(command.chat)
+        }
+    }
+
+    private fun unknownChatResponse(command: TelegramCommand) : TelegramCommandResponse {
+        return TelegramCommandResponse(telegramMessageBuilder.createMessage(
+            command.chatId,
+            "You not registered at this bot yet. Please contact your system administrator for registration."
+        ),
+            State.INIT)
+    }
+
+    private fun unknownCommandResponse(command: TelegramCommand) : TelegramCommandResponse {
+        return TelegramCommandResponse(
+            telegramMessageBuilder.createMessage(
+                command.chatId,
+                "Unknown command. Please use /help to see allowed commands"
+            ),
+            State.INIT
+        )
+    }
+
 }

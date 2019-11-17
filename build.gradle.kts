6f3e894360b9f00649b160995915af48d92dca55
msafonov <msafonovmail@gmail.com>
2019-11-17 14:51:34 +0300

    deps: updated google-http-client-jackson2 to 1.33.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -118,7 +118,7 @@ dependencies {
     implementation("com.atlassian.jira:jira-rest-java-client-core:5.1.2-2bd0a62e")
     implementation("io.atlassian.fugue:fugue:4.7.2")
     implementation("com.google.oauth-client:google-oauth-client:1.30.4")
-    implementation("com.google.http-client:google-http-client-jackson2:1.28.0")
+    implementation("com.google.http-client:google-http-client-jackson2:1.33.0")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 

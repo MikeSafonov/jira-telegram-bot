8cfbea55e906ef1e85d5c867bbb6cdcaff22f4cf
msafonov <msafonovmail@gmail.com>
2019-11-25 09:02:39 +0300

    feat: add logging telegram api response from TelegramApiRequestException

--- src/main/kotlin/com/github/mikesafonov/jira/telegram/endpoint/ExceptionEndpoint.kt
+++ src/main/kotlin/com/github/mikesafonov/jira/telegram/endpoint/ExceptionEndpoint.kt
@@ -3,6 +3,7 @@ package com.github.mikesafonov.jira.telegram.endpoint
 import mu.KotlinLogging
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException
 
 private val logger = KotlinLogging.logger {}
 
@@ -10,10 +11,15 @@ private val logger = KotlinLogging.logger {}
  * @author Mike Safonov
  */
 @RestControllerAdvice
-class ExceptionEndpoint{
+class ExceptionEndpoint {
+
+    @ExceptionHandler(TelegramApiRequestException::class)
+    fun handleTelegramApiException(ex: TelegramApiRequestException) {
+        logger.error("Telegram response: ${ex.apiResponse}", ex)
+    }
 
     @ExceptionHandler(Exception::class)
-    fun handle(ex : Exception){
+    fun handle(ex: Exception) {
         logger.error(ex.message, ex)
     }
 }

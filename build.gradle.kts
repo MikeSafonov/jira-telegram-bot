99859c5a7ac77bff7eed4a67ae374e6d3dc37930
msafonov <safonov.mihail@phoenix-dnr.ru>
2020-03-30 09:42:55 +0300

    deps: update kotlin-logging version to 1.7.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,7 +105,7 @@ dependencies {
     implementation("org.freemarker:freemarker:2.3.29")
     implementation("no.api.freemarker:freemarker-java8:1.3.0")
 
-    implementation("io.github.microutils:kotlin-logging:1.7.7")
+    implementation("io.github.microutils:kotlin-logging:1.7.9")
     implementation("org.apache.logging.log4j:log4j-web")
 
     implementation("io.micrometer:micrometer-core")

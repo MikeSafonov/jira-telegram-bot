3a8fb3b6cc128e8a60f56271e810604f3c66e29d
msafonov <msafonovmail@gmail.com>
2020-12-11 16:46:09 +0300

    deps: update kotlin-logging-jvm to 2.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,7 +105,7 @@ dependencies {
     implementation("org.freemarker:freemarker:2.3.30")
     implementation("no.api.freemarker:freemarker-java8:1.3.0")
 
-    implementation("io.github.microutils:kotlin-logging:1.7.9")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.3")
     implementation("org.apache.logging.log4j:log4j-web")
 
     implementation("io.micrometer:micrometer-core")
